3fd7843c515f0aa3b9b502cb97aa4e5f5bc193ac
blatin <blatin@wearelanding.com>
2014-04-03 14:58:13 -0700

    HITRCMIGRATION-86: Put flag on all content pages and updated to Drupal coding standards

--- oa_notifications.js
+++ oa_notifications.js
@@ -7,7 +7,7 @@
   Drupal.behaviors.oaNotifications = {
     attach: function (context, settings) {
       $('ul#contextual-tabs .flag-wrapper a').removeClass('btn flag');
-      $('ul#contextual-tabs .flag-message, ul#contextual-tabs .flag-throbber').remove();
+      $('ul#contextual-tabs .flag-message, ul#contextual-tabs .flag-throbber, ul#contextual-tabs .flag-wrapper i').remove();
     }
   };
 
